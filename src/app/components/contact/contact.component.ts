ce6192fd218d0b1d3a6dbc4793722efee2cae80e
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-07-17 10:54:10 +0200

    method search

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Category } from 'src/app/models/category.model';
 import { Contact } from 'src/app/models/contact.model';
 import { ApiService } from 'src/app/services/api.service';
@@ -11,45 +12,77 @@ import { ApiService } from 'src/app/services/api.service';
 export class ContactComponent implements OnInit {
 
   contacts: Contact[] | undefined;
-  categories : Category[] | undefined;
+  categories: Category[] | undefined;
+  searchForm: FormGroup;
+  searchError: any;
+  error:any;
 
-  error: any;
 
-  constructor(private service: ApiService) { }
+  constructor(private service: ApiService) {
+    this.searchForm = new FormGroup({
+      keyword: new FormControl()
+    })
+
+  }
 
   ngOnInit(): void {
     this.getAllContacts();
     this.getCategories();
+    this.searchForm.reset();
   }
 
-  searchByCategory(event : any) {
-    let element= event.target || event.srcElement || event.currentTarget;
+  searchByCategory(event: any) {
+    let element = event.target || event.srcElement || event.currentTarget;
     let elementId = element.id
-    this.service.getContactsByCategory(elementId).subscribe( {
-      next: (data) => this.contacts = data ,
-      error : (err) => this.error = err.message,
-      complete: () => this.error= null
-    })}
+    this.service.getContactsByCategory(elementId).subscribe({
+      next: (data) => this.contacts = data,
+      error: (err) => this.error = err.message,
+      complete: () => this.error = null
+    })
+  }
 
   getCategories() {
-   this.service.getCategories().subscribe({
-    next: (data) => this.categories = data ,
-    error : (err) => this.error = err.message,
-    complete: () => this.error= null
-   })
+    this.service.getCategories().subscribe({
+      next: (data) => this.categories = data,
+      error: (err) => this.error = err.message,
+      complete: () => this.error = null
+    })
   }
 
 
-  
+
   getAllContacts() {
     this.service.getContacts().subscribe({
       next: (data) => {
         console.log(data)
-        this.contacts = data},
+        this.contacts = data
+        this.searchError =""
+        this.searchForm.reset();
+      },
       error: (err) => this.error = err.message,
       complete: () => this.error = null
 
     })
+  }
+
+  onSearch(form: FormGroup) {
+    if (form.valid) {
+      this.service.getContactsByNameContains(form.value.keyword).subscribe({
+        next: (data) => {
+          if (data.length === 0) {
+            this.searchError = "Aucun contact trouvé !"
+            this.contacts = [];
+          } else {
+            this.contacts = data
+            this.searchError =""
+          }
+        },
+        error: (err) => this.error = err.message,
+        complete: () => this.error = null
+
+      })
+    }
+
   }
 
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,15 +7,18 @@ import { environment } from 'src/environment/environment';
   providedIn: 'root'
 })
 export class ApiService {
+  getContactsByNameContains(keyword: string) {
+    return this.http.get<Contact[]>(environment.host + "/contacts/search/" + keyword)
+  }
 
 
   getContactsByCategory(id: any) {
-    return this.http.get<Contact[]>(environment.host+"/contacts/category/"+id)
+    return this.http.get<Contact[]>(environment.host + "/contacts/category/" + id)
   }
 
 
   getCategories() {
-   return this.http.get<any>(environment.host+"/category");
+    return this.http.get<any>(environment.host + "/category");
   }
 
   constructor(private http: HttpClient) { }
