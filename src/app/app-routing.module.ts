7dc4e8b06580d9aaa7f0a186aedb7a3bc12430be
Anselme Guillaume <anselmeguillaume@orange.fr>
2023-07-19 10:24:33 +0200

    deconnexion ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { Category } from './models/category.model';
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: 'contacts', component: ContactComponent },
   { path: '', redirectTo: 'contacts', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent},
 
 ]
